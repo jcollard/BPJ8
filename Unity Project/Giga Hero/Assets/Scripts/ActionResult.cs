148301665583305ea1ff3a10d3883cec6a5af278
Joseph Collard <jcollard@users.noreply.github.com>
2021-07-18 12:55:31 -0700

    Organizing code.

---
+++ Unity Project/Giga Hero/Assets/Scripts/ActionResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class ActionResult
+    {
+        public static ActionResult LEVEL_UP = new LevelUpResult();
+        public static ActionResult NOTHING = new ActionResult();
+
+        public static Func<GigaHero, ActionResult> Nothing(Action<GigaHero> action)
+        {
+            return (engine) =>
+            {
+                action.Invoke(engine);
+                return NOTHING;
+            };
+        }
+
+        public virtual void PerformResult(GameState gameState)
+        {
+
+        }
+
+    }
+
+
+    public class LevelUpResult : ActionResult
+    {
+        public override void PerformResult(GameState gameState)
+        {
+            gameState.LevelUp();
+        }
+    }
+
+}
---
+++ Unity Project/Giga Hero/Assets/Scripts/Hero/Hero.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public abstract class Hero
+    {
+
+        public abstract ActionResult Poke();
+        public abstract Hero LevelUp(GameState state);
+
+        public virtual Transition GetTransition()
+        {
+            return null;
+        }
+
+        public abstract void HandleAnimator(Animator animator, GameState gameState);
+    }
+
+    public enum BabyState
+    {
+        HAPPY, PACIFIER, HUNGRY, CRYING, SLEEPING
+    }
+
+    public class Baby : Hero
+    {
+
+        private BabyState _state = BabyState.PACIFIER;
+
+        public override ActionResult Poke()
+        {
+            return ActionResult.NOTHING;
+        }
+
+        public override Hero LevelUp(GameState state)
+        {
+            return this;
+        }
+
+        public override Transition GetTransition()
+        {
+            //TODO
+            return null;
+        }
+
+        public override void HandleAnimator(Animator animator, GameState gameState)
+        {
+            animator.SetBool("isPacifier", true);
+        }
+    }
+    public class Egg : Hero
+    {
+
+        private bool isCracking;
+        private int poked = 0;
+        private float shakeUntil;
+
+        public override ActionResult Poke()
+        {
+            int chance = UnityEngine.Random.Range(0, 100) + poked;
+            if (poked > 3 && chance > 75 && !this.isCracking)
+            {
+                this.isCracking = true;
+                return ActionResult.LEVEL_UP;
+            }
+            poked++;
+            if (Time.time > shakeUntil)
+            {
+                shakeUntil = Time.time + 1.0F;
+            }
+            return ActionResult.NOTHING;
+        }
+
+        public override Hero LevelUp(GameState state)
+        {
+            return new Baby();
+        }
+
+        public override void HandleAnimator(Animator animator, GameState gameState)
+        {
+            animator.SetBool("isCracking", this.isCracking);
+            animator.SetBool("isShaking", shakeUntil > Time.time);
+        }
+
+        public override Transition GetTransition()
+        {
+            return ObjectLookup.EggToBaby;
+        }
+    }
+}
